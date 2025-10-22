2d72c741bec4b5f968bd46dc3c76342a6ad7761e
rweboficial-design <rweboficial@gmail.com>
2025-10-22 16:01:32 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import useStore from '../store'
+
 export default function Header(){
+  const { userType, setUserType } = useStore()
+
+  const resetRole = () => {
+    localStorage.removeItem('userType')
+    setUserType('') // volta para a tela de escolha
+  }
+
   return (
     <header className="header">
       <img src="/logo.png" alt="Logo" height="40"/>
       <h1>Igreja Vota</h1>
+      {userType && (
+        <button onClick={resetRole} style={{marginLeft:'auto'}}>
+          Trocar perfil
+        </button>
+      )}
     </header>
   )
 }
