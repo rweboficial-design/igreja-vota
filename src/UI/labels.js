99c6a32703c7f8ac16758ccdee85ab0a92f89734
rweboficial-design <rweboficial@gmail.com>
2025-10-25 15:18:14 -0300

    Add files via upload

---
+++ src/UI/labels.js
@@ -0,0 +1,19 @@
+const LABELS = {
+  // Membros
+  membersTitle: 'Membros',
+  addMember: 'Adicionar Membro',
+  deleteMember: 'Excluir',
+
+  // Ministérios
+  ministriesTitle: 'Ministérios',
+  addMinistry: 'Incluir ministério',
+  editMinistry: 'Editar',
+  deleteMinistry: 'Excluir',
+
+  // Cargos
+  rolesTitle: 'Cargos',
+  addRole: 'Incluir cargo',
+  editRole: 'Editar',
+  deleteRole: 'Excluir',
+};
+export default LABELS;
